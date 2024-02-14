d1bd35919075953decf623e63c525ee6f5bebd7c
Divineleo2001 <divineleo20@gmail.com>
2024-02-14 13:38:58 +0530

    loading spinners added to all the pages

--- src/app/dashboard/annexures/a/loading.tsx
+++ src/app/dashboard/annexures/a/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen">
       <Spinner />
     </div>
   )
