d4591914a936bd4825db6f2eabe2137d914c456e
Divineleo2001 <divineleo20@gmail.com>
2024-02-19 18:58:37 +0530

    Add loading component for export clearance annexures

---
+++ src/app/dashboard/annexures/b/home-consumption-clearance/resultant-product/loading.tsx
@@ -0,0 +1,12 @@
+import Spinner from '@/components/ui/spinner'
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Spinner />
+    </div>
+  )
+}
+
+export default Loading
