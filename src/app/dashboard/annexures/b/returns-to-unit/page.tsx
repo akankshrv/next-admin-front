22bcf398c4689fefb41d6d36e0f8ee9e547dd023
Divineleo2001 <divineleo20@gmail.com>
2024-02-21 18:50:48 +0530

    AUR-14 <add style fix for returns to unit form component>

--- src/app/dashboard/annexures/b/returns-to-unit/page.tsx
+++ src/app/dashboard/annexures/b/returns-to-unit/page.tsx
@@ -9,59 +9,59 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import React from 'react'
+import React from "react";
+import { Textarea } from "@/components/ui/textarea";
 
 const ReturnsToUnitForm = () => {
   return (
-   <>
-   <Card className="max-w-4xl mx-auto my-10 bg-gray-100 p-20">
+    <>
+      <Card className="max-w-4xl mx-auto my-5 bg-gray-100 p-5">
         <CardHeader>
-
           <CardTitle>
-            <h1 className="text-2xl">Reeturns to unit after job work</h1>
+            <h1 className="text-2xl">Returns to unit after job work</h1>
           </CardTitle>
         </CardHeader>
 
-        <CardContent>
-          <form className="">
-            <div className="flex gap-2 max-w-xl">
+        <CardContent className="mx-auto">
+          <form className="space-y-4">
+            <div className="flex gap-2">
               <div className="flex-1 ">
                 <Label>Date</Label>
                 <Input type="date" />
-              </div> 
+              </div>
               <div className="flex-1">
                 <Label>Time of return:</Label>
-                <Input type="time"/>
+                <Input type="time" />
               </div>
-              </div>
-            <div className="max-w-xl">
+            </div>
+
+            <div className="flex-1 space-y-2">
               <Label>Description of goods</Label>
-              <Input placeholder="Description of goods" />
+              <Textarea className="pb-20" placeholder="Description of goods" />
             </div>
+            <div className="flex gap-2 pt-1">
+              <div className="flex-1">
+                <Label>Quality with UQC:</Label>
+                <Input placeholder="Quality with UQC" />
+              </div>
+              <div className="flex-1">
+                <Label>Value:</Label>
+                <Input placeholder="value" />
+              </div>
 
-            <div className="max-w-xl">
-              <Label>Quality with UQC:</Label>
-              <Input placeholder="Quality with UQC" />
+              <div className="flex-1">
+                <Label>Delivery Challan No:</Label>
+                <Input placeholder="Delivery Challan No" />
+              </div>
             </div>
-            <div className="max-w-xl">
-              <Label>Value:</Label>
-              <Input placeholder="value" />
-            </div>
-      
-            <div className="max-w-xl">
-              <Label>Delivery Challan No:</Label>
-              <Input placeholder="Delivery Challan No" />
-            </div>
-
-            
           </form>
         </CardContent>
-        <CardFooter className="flex justify-end">
-          <Button>Save</Button>
+        <CardFooter className="flex justify-start">
+          <Button className="w-96 ">Save</Button>
         </CardFooter>
       </Card>
-   </>
-  )
-}
+    </>
+  );
+};
 
-export default ReturnsToUnitForm
+export default ReturnsToUnitForm;
